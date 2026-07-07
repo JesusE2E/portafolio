a2f283b36b84906c4b023c292485217c37c08c04
Jesus Vazquez <jesusvb314@gmail.com>
2026-07-07 17:28:26 -0600

    pagina de inicio9

---
+++ static/jss/modal.js
@@ -0,0 +1,9 @@
+
+  function abrirModal() {
+    document.getElementById('miModal').style.display = 'block';
+  }
+  function cerrarModal() {
+    document.getElementById('miModal').style.display = 'none';
+  }
+
+
